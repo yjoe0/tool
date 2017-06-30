554b7f7d4daebee89a2d6076af520ecaf6d17e4c
yjoe0 <970423419@qq.com>
2017-06-30 22:04:37 +0800

    '.gitignore'

--- Runtime/Data/_fields/flight.index.php
+++
@@ -1,1 +0,0 @@
-a:8:{i:0;s:2:"id";i:1;s:5:"title";i:2;s:6:"magnet";i:3;s:5:"md5bt";i:4;s:5:"cover";i:5;s:12:"downloadtime";s:3:"_pk";s:2:"id";s:5:"_type";a:6:{s:2:"id";s:6:"int(6)";s:5:"title";s:12:"varchar(128)";s:6:"magnet";s:13:"varchar(1020)";s:5:"md5bt";s:11:"varchar(32)";s:5:"cover";s:13:"varchar(1023)";s:12:"downloadtime";s:6:"int(5)";}}
